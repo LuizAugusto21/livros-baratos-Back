505d033cc37769b4020b7dca1621f5bd9971261a
Luiz Augusto <luis2848@gmail.com>
2024-06-30 15:07:32 -0300

    Modelagem Livro

---
+++ src/main/java/com/example/livros_baratos_back/model/Condicao.java
@@ -0,0 +1,7 @@
+package com.example.livros_baratos_back.model;
+
+public enum Condicao {
+    NOVO,
+    SEMINOVO,
+    USADO
+}
---
+++ src/main/java/com/example/livros_baratos_back/model/Livro.java
@@ -0,0 +1,73 @@
+package com.example.livros_baratos_back.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String nomeAutor;
+
+    private double preco;
+
+    @Enumerated(EnumType.STRING)
+    private Condicao condicao;
+
+    // Getters e Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Condicao getCondicao() {
+        return condicao;
+    }
+
+    public void setCondicao(Condicao condicao) {
+        this.condicao = condicao;
+    }
+}
