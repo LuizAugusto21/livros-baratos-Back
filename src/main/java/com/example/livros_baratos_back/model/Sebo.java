35cefba4b1ae2b3f00805d18b8c0a11ccebb72c0
Pedro Henrique <pedrosales2806@gmail.com>
2024-06-30 20:28:37 -0300

    Mudando de branch

---
+++ src/main/java/com/example/livros_baratos_back/model/Sebo.java
@@ -0,0 +1,10 @@
+package com.example.livros_baratos_back.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Sebo {
+    @Id
+    
+}
---
+++ src/main/java/com/example/livros_baratos_back/repository/SeboRepository.java
@@ -0,0 +1,9 @@
+package com.example.livros_baratos_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeboRepository extends JpaRepository<Object, Object> {
+    
+}
