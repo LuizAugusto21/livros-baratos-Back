a5acaf7a12ff9cd41209b3c8e13c1028f7342ec6
Pedro Henrique <pedrosales2806@gmail.com>
2024-07-01 17:50:36 -0300

    Atualização das requisições

--- src/main/java/com/example/livros_baratos_back/controller/SeboController.java
+++ src/main/java/com/example/livros_baratos_back/controller/SeboController.java
@@ -1,11 +1,15 @@
 package com.example.livros_baratos_back.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +20,12 @@ import com.example.livros_baratos_back.service.SeboService;
 @RequestMapping("/sebos")
 public class SeboController {
 
+    private final SeboService seboService;
+
     @Autowired
-    private SeboService seboService;
-    
-    // CRUD do SEBO
+    public SeboController(SeboService seboService) {
+        this.seboService = seboService;
+    }
 
     @GetMapping
     public List<Sebo> getAllSebos(){
@@ -27,10 +33,35 @@ public class SeboController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Sebo> getSeboById(@PathVariable Long id){
-        return seboService.findById(id);
+    public ResponseEntity<Sebo> getSeboById(@PathVariable Long id){
+        return seboService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    
+    @PostMapping
+    public Sebo createSebo(@RequestBody Sebo sebo){
+        return seboService.save(sebo);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Sebo> updateSebo(@PathVariable Long id, @RequestBody Sebo sebo){
+        return seboService.findById(id)
+                .map(existingSebo -> {
+                    sebo.setId(existingSebo.getId());
+                    return ResponseEntity.ok(seboService.save(sebo));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSebo(@PathVariable Long id){
+        if(seboService.findById(id).isPresent()){
+            seboService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
